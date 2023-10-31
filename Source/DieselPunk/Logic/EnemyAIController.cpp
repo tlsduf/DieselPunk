3ef93fa78253ce0dd99cc1305e99fc363a07d94c
tlsduf <ehgns0765@gmail.com>
2023-10-31 18:56:53 +0900

    [신열] 1031 내비게이션 관련 설정

--- Source/DieselPunk/Logic/EnemyAIController.cpp
+++ Source/DieselPunk/Logic/EnemyAIController.cpp
@@ -4,20 +4,26 @@
 #include "..\Character\CharacterPC.h"
 
 #include <Kismet/GameplayStatics.h>
-#include <BehaviorTree/BehaviorTree.h>
 #include <BehaviorTree/BlackboardComponent.h>
 #include <Math/Vector.h>
+#include <Navigation/PathFollowingComponent.h>
 
 const FName AEnemyAIController::TargetKey(TEXT("Target"));
 
 void AEnemyAIController::BeginPlay()
 {
     Super::BeginPlay();
+
+    
+   // GetPathFollowingComponent()->IsBlocked();
 }
 
 void AEnemyAIController::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    //UE_VLOG(GetOwner(), LogPathFollowing, Log, TEXT("OnPathFinished: %s"), *Result.ToString());
+
 }
 
 bool AEnemyAIController::IsDead() const
