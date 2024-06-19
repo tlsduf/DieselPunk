c9814d8ba4618593c11c7be188e7963b09e80937
theqqqq <kduck119@gmail.com>
2024-06-19 16:21:56 +0900

    [더큐] 오타 수정

---
+++ Source/DieselPunk/Skill/SkillNPC/SuicideBomb.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../SkillBase.h"
+#include "SuicideBomb.generated.h"
+
+/**
+ *
+ */
+
+UCLASS()
+class DIESELPUNK_API USuicideBomb : public USkillBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category="MYDP_Collision")
+	double Radius = 1000.0;
+	
+public:
+	// 생성자
+	USuicideBomb();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void AbilityStart(AActor* InTarget = nullptr) override;
+
+	virtual void AbilityShot(AActor* InTarget = nullptr) override;
+};
